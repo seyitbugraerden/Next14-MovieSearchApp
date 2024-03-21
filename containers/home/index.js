ed2202f063c4a889b9a9b9c5a04fca90743f9b33
seyitbugraerden <seyitbugraerden@gmail.com>
2024-03-21 21:21:49 +0300

    End Of HomePage

--- containers/home/index.js
+++ containers/home/index.js
@@ -3,11 +3,20 @@ import FeaturedMovie from "@/components/featured-movie";
 import Categories from "@/components/categories";
 import Movies from "@/mocks/movies.json";
 import Genres from "@/mocks/genres.json";
+import MoviesSection from "@/components/movies-section";
 function HomeContainer() {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
-      <Categories categories={Genres.genres.slice(0,5)} />
+      <Categories categories={Genres.genres.slice(0, 5)} />
+      <MoviesSection
+        title='Popular Films'
+        movies={Movies.results.slice(0,12)}
+      />
+      <MoviesSection
+        title='Your Favorites'
+        movies={Movies.results.slice(12,24)}
+      />
     </div>
   );
 }
