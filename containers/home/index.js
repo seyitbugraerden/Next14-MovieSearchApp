59d946f8ac534215d7fafeb26308cca8be80530a
seyitbugraerden <seyitbugraerden@gmail.com>
2024-03-21 21:08:10 +0300

    Categories

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import FeaturedMovie from "@/components/featured-movie";
+import Categories from "@/components/categories";
 import Movies from "@/mocks/movies.json";
+import Genres from "@/mocks/genres.json";
 function HomeContainer() {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
+      <Categories categories={Genres.genres.slice(0,5)} />
     </div>
   );
 }
