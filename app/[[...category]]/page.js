7212babf74c81376c9e0df76db718b7cabc90b0a
seyitbugraerden <seyitbugraerden@gmail.com>
2024-03-22 09:14:39 +0300

    Fixing The Bugs

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -1,37 +1,20 @@
-import React from "react";
 import HomeContainer from "@/containers/home";
+import React from "react";
 
-import {
-  fetchPopularMovies,
-  fetchTopRatedMovies,
-  fetchGenres,
-  fetchMoviesByGenre,
-} from "@/services/movie";
+import Movies from "@/mocks/movies.json";
 
-async function HomePage({ params }) {
-  const pagePromises = [
-    fetchPopularMovies(),
-    fetchTopRatedMovies(),
-    fetchGenres(),
-  ];
-
-  if (!!params.category?.length) {
-    pagePromises.push(fetchMoviesByGenre(params.category[0]));
+function HomePage({ params }) {
+  let selectedCategory;
+  if (params.category?.length > 0) {
+    selectedCategory = true;
   }
-
-  const [popularMovies, topRatedMovies, genres, selectedCategoryMovies] =
-    await Promise.all(pagePromises);
-
   return (
-    <HomeContainer
-      categories={genres}
-      popularMovies={popularMovies}
-      topRatedMovies={topRatedMovies}
-      selectedCategory={{
-        id: params.category?.[0] ?? "",
-        movies: selectedCategoryMovies ?? [],
-      }}
-    />
+    <>
+      <HomeContainer selectedCategory={{
+        id: params.category ? params.category[0] ?? '' : '',
+        movies: selectedCategory ? Movies.results.slice(0, 7) : []
+      }}/>
+    </>
   );
 }
 
--- containers/home/index.js
+++ containers/home/index.js
@@ -4,18 +4,28 @@ import Categories from "@/components/categories";
 import Movies from "@/mocks/movies.json";
 import Genres from "@/mocks/genres.json";
 import MoviesSection from "@/components/movies-section";
-function HomeContainer() {
+function HomeContainer({ selectedCategory }) {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
+      {selectedCategory.movies.length > 0 && (
+        <MoviesSection
+          title={
+            Genres.genres.find((item) => `${item.id}` === selectedCategory.id)
+              .name
+          }
+          movies={selectedCategory.movies}
+        />
+      )}
+
       <MoviesSection
-        title='Popular Films'
-        movies={Movies.results.slice(0,12)}
+        title="Popular Movies"
+        movies={Movies.results.slice(0, 12)}
       />
       <MoviesSection
-        title='Your Favorites'
-        movies={Movies.results.slice(12,24)}
+        title="Your Favorites"
+        movies={Movies.results.slice(12, 24)}
       />
     </div>
   );
